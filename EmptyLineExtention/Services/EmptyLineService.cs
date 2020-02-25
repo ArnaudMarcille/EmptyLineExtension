fccca8b9466203720c5da5fdad3e4b7d27df878e
ArnaudMarcille <arnaud.marcille@hotmail.fr>
2020-02-25 16:34:40 +0100

    Remove undo context (causes issues with ctrl-r-r command)

--- EmptyLineExtention/Services/EmptyLineService.cs
+++ EmptyLineExtention/Services/EmptyLineService.cs
@@ -1,5 +1,4 @@
-﻿using EmptyLineExtention.Core.Localization;
-using EnvDTE;
+﻿using EnvDTE;
 
 namespace EmptyLineExtention.Services
 {
@@ -21,67 +20,57 @@ namespace EmptyLineExtention.Services
             // Get the current Doc
             TextDocument activeDoc = document.Object() as TextDocument;
 
-            try
+            // set default start point
+            int startPoint = 1;
+            int endPoint = activeDoc.EndPoint.Line;
+            // initialise edit point
+            var editPoint = activeDoc.CreateEditPoint(activeDoc.StartPoint);
+
+            // check if there is a selection
+            if (!activeDoc.Selection.IsEmpty && CanUseSelection)
             {
-                // open undo context
-                dte.UndoContext.Open(Labels.Undo_Removed);
-                // set default start point
-                int startPoint = 1;
-                int endPoint = activeDoc.EndPoint.Line;
-                // initialise edit point
-                var editPoint = activeDoc.CreateEditPoint(activeDoc.StartPoint);
-
-                // check if there is a selection
-                if (!activeDoc.Selection.IsEmpty && CanUseSelection)
-                {
-                    startPoint = activeDoc.Selection.TopLine;
-                    endPoint = activeDoc.Selection.BottomLine;
-                    editPoint.LineDown(startPoint - 1);
-                }
-
-                // remove all multiple space between start and end point
-                int numberOfEmptyLines = 0;
-                for (int number = startPoint; number <= endPoint; number++)
-                {
-                    bool lineDeleted = false;
-                    string line = editPoint.GetLines(number, number + 1);
-                    editPoint.CreateEditPoint();
-                    if (string.IsNullOrWhiteSpace(line))
-                    {
-                        numberOfEmptyLines++;
-
-                        if (numberOfEmptyLines > AllowedLines)
-                        {
-                            editPoint.StartOfLine();
-                            // Delete "spaces"
-                            editPoint.Delete(line.Length);
-                            // Delete breakline
-                            editPoint.Delete(-1);
-
-                            lineDeleted = true;
-
-                            if (!activeDoc.Selection.IsEmpty)
-                                endPoint = activeDoc.Selection.BottomLine;
-                            else
-                                endPoint = activeDoc.EndPoint.Line;
-                        }
-                    }
-                    else
-                    {
-                        numberOfEmptyLines = 0;
-                    }
-
-                    if (lineDeleted)
-                    {
-                        number--;
-                    }
-                    editPoint.LineDown(1);
-                }
+                startPoint = activeDoc.Selection.TopLine;
+                endPoint = activeDoc.Selection.BottomLine;
+                editPoint.LineDown(startPoint - 1);
             }
-            finally
+
+            // remove all multiple space between start and end point
+            int numberOfEmptyLines = 0;
+            for (int number = startPoint; number <= endPoint; number++)
             {
-                // close undo context
-                dte.UndoContext.Close();
+                bool lineDeleted = false;
+                string line = editPoint.GetLines(number, number + 1);
+                editPoint.CreateEditPoint();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    numberOfEmptyLines++;
+
+                    if (numberOfEmptyLines > AllowedLines)
+                    {
+                        editPoint.StartOfLine();
+                        // Delete "spaces"
+                        editPoint.Delete(line.Length);
+                        // Delete breakline
+                        editPoint.Delete(-1);
+
+                        lineDeleted = true;
+
+                        if (!activeDoc.Selection.IsEmpty)
+                            endPoint = activeDoc.Selection.BottomLine;
+                        else
+                            endPoint = activeDoc.EndPoint.Line;
+                    }
+                }
+                else
+                {
+                    numberOfEmptyLines = 0;
+                }
+
+                if (lineDeleted)
+                {
+                    number--;
+                }
+                editPoint.LineDown(1);
             }
         }
     }
