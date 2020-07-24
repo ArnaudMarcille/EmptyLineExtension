1d6c4fdc37d41bc9ad2b64400ba1d1546ea983cf
ArnaudMarcille <arnaud.marcille@hotmail.fr>
2020-07-24 14:10:00 +0200

    Update

--- EmptyLineExtention/Core/Settings/OptionPageExtensions.cs
+++ EmptyLineExtention/Core/Settings/OptionPageExtensions.cs
@@ -3,13 +3,26 @@ using Newtonsoft.Json;
 
 namespace EmptyLineExtention.Core.Settings
 {
+    /// <summary>
+    /// Option page extension
+    /// </summary>
     public static class OptionPageExtensions
     {
+        /// <summary>
+        /// Get setting items
+        /// </summary>
+        /// <param name="optionPage"></param>
+        /// <returns></returns>
         public static List<SettingItem> GetSettingItems(this OptionPage optionPage)
         {
             return JsonConvert.DeserializeObject<List<SettingItem>>(optionPage.FilesConfigurations);
         }
 
+        /// <summary>
+        /// Set setting items
+        /// </summary>
+        /// <param name="optionPage"></param>
+        /// <param name="items"></param>
         public static void SetSettingItems(this OptionPage optionPage, List<SettingItem> items)
         {
             optionPage.FilesConfigurations = JsonConvert.SerializeObject(items);
