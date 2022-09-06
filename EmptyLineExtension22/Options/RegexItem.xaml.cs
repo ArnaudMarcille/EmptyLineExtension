a6ad2de439028ffb169bab9032f11429b2c0cb85
arnaud7394 <marcille.arnaud@gmail.com>
2022-09-06 10:33:25 +0200

    Update deisng of regex management

---
+++ EmptyLineExtension22/Options/RegexItem.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmptyLineExtension22.Options
+{
+    /// <summary>
+    /// Interaction logic for RegexItem.xaml
+    /// </summary>
+    public partial class RegexItem : UserControl
+    {
+        public RegexItem()
+        {
+
+            InitializeComponent();
+        }
+
+        private void tbx_AllowedLines_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
